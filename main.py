37cdfc47c698c222b3019b479dade3a27676e1a3
Okafor Onyekachukwu <okaforbestkid@gmail.com>
2023-03-12 11:30:12 +0100

    deployment

---
+++ main.py
@@ -0,0 +1,80 @@
+from fastapi import FastAPI
+from models import Fruits, updateFruits
+
+app = FastAPI()
+
+fruitsSeen : list[Fruits] = [
+    Fruits(
+      name= 'Pineapple',
+    day_first_seen= '12th October, 2020',
+
+    condition='ripe',
+    location= 'Amawbia'
+    ),
+    Fruits(
+      name='Apple',
+    day_first_seen= '12th January, 2020',
+    condition= 'overripe',
+    location='Okpunno'
+    ),
+    Fruits(
+      name='Strawberry',
+    day_first_seen= '12th November, 2020',
+    condition='ripe',
+    location='ifite'
+    ),
+    Fruits(
+      name= 'Mango',
+    day_first_seen='12th August, 2020',
+    condition='unripe',
+    location='Ngozika'
+    ),
+    Fruits(
+    name= 'Udala',
+    day_first_seen='12th June, 2020',
+    condition='ripe',
+    location='Ifite'
+    ),
+]
+
+
+@app.get("/")
+async def fruitList():
+    return fruitsSeen
+
+
+
+@app.get("/search/{search_name}")
+async def singleFruit(search_name):
+    for fruit in fruitsSeen:
+        if fruit.name == search_name:
+            fruits = fruit
+            return fruits
+        
+@app.post('/add_fruit/')
+async def q(fruit: Fruits):
+    fruitsSeen.append(fruit)
+    return fruit
+
+@app.put('/edit/{fruit_name}')
+async def editFruit(fruit: updateFruits, fruit_name: str):
+    print(fruit_name)
+    for i in fruitsSeen:
+        if i.name == fruit_name:
+            if fruit.condition is not None:
+                i.condition = fruit.condition
+                
+            if fruit.day_last_seen is not None:
+                i.day_last_seen = fruit.day_last_seen
+            if fruit.price is not None:
+                i.price = fruit.price
+            if fruit.image is not None:
+                i.image = fruit.image
+            return
+        
+@app.delete("/delete/{fruit_name}")
+async def deleteFruit(fruit_name):
+    for fruit in fruitsSeen:
+        if fruit.name == fruit_name:
+            fruitsSeen.remove(fruit)
+            return
---
+++ models.py
@@ -0,0 +1,28 @@
+from pydantic import BaseModel, UUID4
+from typing import Optional
+# from fastapi import
+
+from enum import Enum
+
+class Condition(str, Enum):
+    ripe = 'ripe'
+    unripe = 'unripe'
+    overripe = 'overripe'
+
+class Fruits(BaseModel):
+    # id: UUID4()
+    name: str
+    day_last_seen: Optional[str]
+    price: Optional[str]
+    day_first_seen: str
+    condition: Condition
+    location: str
+    image: Optional[str]
+
+
+class updateFruits(BaseModel):
+    # id: UUID4()
+    day_last_seen: Optional[str]
+    price: Optional[str]
+    condition: Optional[str]
+    image: Optional[str]
